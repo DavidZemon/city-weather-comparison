8284313c6309a42211a4cdec2324bd1e2584009c
David Zemon <david@zemon.name>
2023-06-19 19:08:55 -0500

    chore: Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,114 @@
-import React, {useState} from 'react';
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import {CityName} from "./utils";
-import {MyChart, Range} from "./MyChart";
+import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { CityName } from "./utils";
+import { MyChart, Range } from "./MyChart";
 
 function App() {
-    const [showApparentHighs, setApparentHighs] = useState(true);
-    const [showApparentLows, setShowApparentLows] = useState(true);
-    const [showHighs, setHighs] = useState(false);
-    const [showLows, setLows] = useState(false);
-    const [red, setRed] = useState<Range>({color: "red", from: 100, to: 200});
-    const [green, setGreen] = useState<Range>({color: "green", from: 65, to: 85});
-    const [blue, setBlue] = useState<Range>({color: "blue", from: 35, to: -100});
+  const [showApparentHighs, setApparentHighs] = useState(true);
+  const [showApparentLows, setShowApparentLows] = useState(true);
+  const [showHighs, setHighs] = useState(false);
+  const [showLows, setLows] = useState(false);
+  const [red, setRed] = useState<Range>({ color: "red", from: 100, to: 200 });
+  const [green, setGreen] = useState<Range>({
+    color: "green",
+    from: 65,
+    to: 85,
+  });
+  const [blue, setBlue] = useState<Range>({
+    color: "blue",
+    from: 35,
+    to: -100,
+  });
 
-    return (
-        <Container>
-            <Form>
-                <Form.Switch
-                    id='apparentHighs'
-                    title='Show daily apparent high temperatures (heat index / wind chill)'
-                    label='Daily Apparent Highs'
-                    defaultChecked={showApparentHighs}
-                    onClick={(e) => setApparentHighs(!showApparentHighs)}
-                />
+  return (
+    <Container>
+      <Form>
+        <Form.Switch
+          id="apparentHighs"
+          title="Show daily apparent high temperatures (heat index / wind chill)"
+          label="Daily Apparent Highs"
+          defaultChecked={showApparentHighs}
+          onClick={(e) => setApparentHighs(!showApparentHighs)}
+        />
 
-                <Form.Switch
-                    id='apparentLows'
-                    title='Show daily apparent low temperatures (heat index / wind chill)'
-                    label='Daily Apparent Lows'
-                    defaultChecked={showApparentLows}
-                    onClick={(e) => setShowApparentLows(!showApparentLows)}
-                />
+        <Form.Switch
+          id="apparentLows"
+          title="Show daily apparent low temperatures (heat index / wind chill)"
+          label="Daily Apparent Lows"
+          defaultChecked={showApparentLows}
+          onClick={(e) => setShowApparentLows(!showApparentLows)}
+        />
 
-                <Form.Switch
-                    id='highs'
-                    title='Show daily high temperatures'
-                    label='Daily Highs'
-                    defaultChecked={showHighs}
-                    onClick={(e) => setHighs(!showHighs)}
-                />
+        <Form.Switch
+          id="highs"
+          title="Show daily high temperatures"
+          label="Daily Highs"
+          defaultChecked={showHighs}
+          onClick={(e) => setHighs(!showHighs)}
+        />
 
-                <Form.Switch
-                    id='lows'
-                    title='Show daily low temperatures'
-                    label='Daily Lows'
-                    defaultChecked={showLows}
-                    onClick={(e) => setLows(!showLows)}
-                />
-            </Form>
+        <Form.Switch
+          id="lows"
+          title="Show daily low temperatures"
+          label="Daily Lows"
+          defaultChecked={showLows}
+          onClick={(e) => setLows(!showLows)}
+        />
+      </Form>
 
-            {
-                ([
-                    [red, setRed],
-                    [green, setGreen],
-                    [blue, setBlue]
-                ] as [Range, (range: Range) => void][])
-                    .map(
-                        ([value, setter]) => <Row>
-                            <Col>
-                                <input
-                                    value={value.color}
-                                    onChange={(e) => setter({...value, color: e.target.value.trim()})}
-                                />
-                            </Col>
+      {(
+        [
+          [red, setRed],
+          [green, setGreen],
+          [blue, setBlue],
+        ] as [Range, (range: Range) => void][]
+      ).map(([value, setter]) => (
+        <Row>
+          <Col>
+            <input
+              value={value.color}
+              onChange={(e) =>
+                setter({ ...value, color: e.target.value.trim() })
+              }
+            />
+          </Col>
 
-                            <Col>
-                                <input
-                                    value={value.from}
-                                    onChange={
-                                        (e) => setter({...value, from: Number.parseInt(e.target.value.trim())})
-                                    }
-                                />
-                            </Col>
+          <Col>
+            <input
+              value={value.from}
+              onChange={(e) =>
+                setter({
+                  ...value,
+                  from: Number.parseInt(e.target.value.trim()),
+                })
+              }
+            />
+          </Col>
 
-                            <Col>
-                                <input
-                                    value={value.to}
-                                    onChange={
-                                        (e) => setter({...value, to: Number.parseInt(e.target.value.trim())})
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                    )
-            }
+          <Col>
+            <input
+              value={value.to}
+              onChange={(e) =>
+                setter({ ...value, to: Number.parseInt(e.target.value.trim()) })
+              }
+            />
+          </Col>
+        </Row>
+      ))}
 
-            {
-                (
-                    [
-                        "St. Louis",
-                        "Atlanta",
-                        "Mobile"
-                    ] as CityName[]
-                )
-                    .map(
-                        (cityName) =>
-                            <MyChart
-                                key={cityName}
-                                cityName={cityName}
-                                showApparentHighs={showApparentHighs}
-                                showApparentLows={showApparentLows}
-                                showActualHighs={showHighs}
-                                showActualLows={showLows}
-                                ranges={[red, green, blue]}
-                            />
-                    )
-            }
-        </Container>
-    );
+      {(["St. Louis", "Atlanta", "Mobile"] as CityName[]).map((cityName) => (
+        <MyChart
+          key={cityName}
+          cityName={cityName}
+          showApparentHighs={showApparentHighs}
+          showApparentLows={showApparentLows}
+          showActualHighs={showHighs}
+          showActualLows={showLows}
+          ranges={[red, green, blue]}
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default App;
--- src/MyChart.tsx
+++ src/MyChart.tsx
@@ -1,78 +1,97 @@
-import {CityName, get, Results} from "./utils";
-import React, {useState} from "react";
-import {Button, Row} from "react-bootstrap";
+import { CityName, get, Results } from "./utils";
+import React, { useState } from "react";
+import { Button, Row } from "react-bootstrap";
 import HighchartsReact from "highcharts-react-official";
-import Highcharts from 'highcharts'
+import Highcharts from "highcharts";
 
 export interface Range {
-    color: string;
-    from: number;
-    to: number;
+  color: string;
+  from: number;
+  to: number;
 }
 
 interface Props {
-    cityName: CityName;
-    showApparentHighs: boolean;
-    showApparentLows: boolean;
-    showActualHighs: boolean;
-    showActualLows: boolean;
-    ranges: Range[];
+  cityName: CityName;
+  showApparentHighs: boolean;
+  showApparentLows: boolean;
+  showActualHighs: boolean;
+  showActualLows: boolean;
+  ranges: Range[];
 }
 
-export function MyChart(
+export function MyChart({
+  cityName,
+  showApparentHighs,
+  showApparentLows,
+  showActualHighs,
+  showActualLows,
+  ranges,
+}: Props) {
+  const [results, setResults] = useState<Results | undefined>();
+
+  const data: { name: string; data: number[] }[] = [
     {
-        cityName,
-        showApparentHighs,
-        showApparentLows,
-        showActualHighs,
-        showActualLows,
-        ranges,
-    }: Props
-) {
-    const [results, setResults] = useState<Results | undefined>();
+      name: "Apparent High",
+      data:
+        showApparentHighs && results?.daily
+          ? results?.daily?.apparent_temperature_max
+          : [],
+    },
+    {
+      name: "Apparent Low",
+      data:
+        showApparentLows && results?.daily
+          ? results?.daily?.apparent_temperature_min
+          : [],
+    },
+    {
+      name: "High",
+      data:
+        showActualHighs && results?.daily
+          ? results?.daily?.temperature_2m_max
+          : [],
+    },
+    {
+      name: "Low",
+      data:
+        showActualLows && results?.daily
+          ? results?.daily?.temperature_2m_min
+          : [],
+    },
+  ];
 
-    const data: { name: string, data: number[] }[] = [
-        {
-            name: "Apparent High",
-            data: showApparentHighs && results?.daily ? results?.daily?.apparent_temperature_max : []
-        },
-        {
-            name: "Apparent Low",
-            data: showApparentLows && results?.daily ? results?.daily?.apparent_temperature_min : []
-        },
-        {
-            name: "High",
-            data: showActualHighs && results?.daily ? results?.daily?.temperature_2m_max : []
-        },
-        {
-            name: "Low",
-            data: showActualLows && results?.daily ? results?.daily?.temperature_2m_min : []
-        },
-    ];
-
-
-    return <Row>
-        {!results && <Button onClick={async () => {
-            const results = await get({cityName});
+  return (
+    <Row>
+      {!results && (
+        <Button
+          onClick={async () => {
+            const results = await get({ cityName });
             setResults(results);
-        }}>Load {cityName}</Button>}
+          }}
+        >
+          Load {cityName}
+        </Button>
+      )}
 
-        {results && <HighchartsReact
-            highcharts={Highcharts}
-            options={{
-                title: {text: cityName},
-                xAxis: {
-                    "categories": results?.daily?.time
-                },
-                yAxis: {
-                    name: "Temperature (F)",
-                    min: 0,
-                    max: 110,
-                    tickAmount: 12,
-                    plotBands: ranges
-                },
-                series: data
-            }}
-        />}
-    </Row>;
+      {results && (
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={{
+            title: { text: cityName },
+            xAxis: {
+              categories: results?.daily?.time,
+            },
+            yAxis: {
+              name: "Temperature (F)",
+              min: 0,
+              max: 110,
+              tickAmount: 12,
+              plotBands: ranges,
+            },
+            series: data,
+          }}
+        />
+      )}
+    </Row>
+  );
 }
