f0e582d95fb2ef6d52d37c7b4bf08a04419abfc8
David Zemon <david@zemon.name>
2023-06-19 21:24:57 -0500

    feat: Add accordions to group UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Accordion, Col, Container, Form, Row } from "react-bootstrap";
 import { CityName } from "./utils";
 import { MyChart, Range } from "./MyChart";
 
@@ -22,91 +22,115 @@ function App() {
 
   return (
     <Container>
-      <Form>
-        <Form.Switch
-          id="apparentHighs"
-          title="Show daily apparent high temperatures (heat index / wind chill)"
-          label="Daily Apparent Highs"
-          defaultChecked={showApparentHighs}
-          onClick={(e) => setApparentHighs(!showApparentHighs)}
-        />
+      <Accordion defaultActiveKey={["data"]} alwaysOpen={true}>
+        <Accordion.Item eventKey="dataConfig">
+          <Accordion.Header>Data Configuration</Accordion.Header>
 
-        <Form.Switch
-          id="apparentLows"
-          title="Show daily apparent low temperatures (heat index / wind chill)"
-          label="Daily Apparent Lows"
-          defaultChecked={showApparentLows}
-          onClick={(e) => setShowApparentLows(!showApparentLows)}
-        />
+          <Accordion.Body>
+            <Form>
+              <Form.Switch
+                id="apparentHighs"
+                title="Show daily apparent high temperatures (heat index / wind chill)"
+                label="Daily Apparent Highs"
+                defaultChecked={showApparentHighs}
+                onClick={(e) => setApparentHighs(!showApparentHighs)}
+              />
 
-        <Form.Switch
-          id="highs"
-          title="Show daily high temperatures"
-          label="Daily Highs"
-          defaultChecked={showHighs}
-          onClick={(e) => setHighs(!showHighs)}
-        />
+              <Form.Switch
+                id="apparentLows"
+                title="Show daily apparent low temperatures (heat index / wind chill)"
+                label="Daily Apparent Lows"
+                defaultChecked={showApparentLows}
+                onClick={(e) => setShowApparentLows(!showApparentLows)}
+              />
 
-        <Form.Switch
-          id="lows"
-          title="Show daily low temperatures"
-          label="Daily Lows"
-          defaultChecked={showLows}
-          onClick={(e) => setLows(!showLows)}
-        />
-      </Form>
+              <Form.Switch
+                id="highs"
+                title="Show daily high temperatures"
+                label="Daily Highs"
+                defaultChecked={showHighs}
+                onClick={(e) => setHighs(!showHighs)}
+              />
 
-      {(
-        [
-          [red, setRed],
-          [green, setGreen],
-          [blue, setBlue],
-        ] as [Range, (range: Range) => void][]
-      ).map(([value, setter]) => (
-        <Row>
-          <Col>
-            <input
-              value={value.color}
-              onChange={(e) =>
-                setter({ ...value, color: e.target.value.trim() })
-              }
-            />
-          </Col>
+              <Form.Switch
+                id="lows"
+                title="Show daily low temperatures"
+                label="Daily Lows"
+                defaultChecked={showLows}
+                onClick={(e) => setLows(!showLows)}
+              />
+            </Form>
+          </Accordion.Body>
+        </Accordion.Item>
 
-          <Col>
-            <input
-              value={value.from}
-              onChange={(e) =>
-                setter({
-                  ...value,
-                  from: Number.parseInt(e.target.value.trim()),
-                })
-              }
-            />
-          </Col>
+        <Accordion.Item eventKey="chartConfig">
+          <Accordion.Header>Chart Configuration</Accordion.Header>
+          <Accordion.Body>
+            {(
+              [
+                [red, setRed],
+                [green, setGreen],
+                [blue, setBlue],
+              ] as [Range, (range: Range) => void][]
+            ).map(([value, setter]) => (
+              <Row>
+                <Col>
+                  <input
+                    value={value.color}
+                    onChange={(e) =>
+                      setter({ ...value, color: e.target.value.trim() })
+                    }
+                  />
+                </Col>
 
-          <Col>
-            <input
-              value={value.to}
-              onChange={(e) =>
-                setter({ ...value, to: Number.parseInt(e.target.value.trim()) })
-              }
-            />
-          </Col>
-        </Row>
-      ))}
+                <Col>
+                  <input
+                    value={value.from}
+                    onChange={(e) =>
+                      setter({
+                        ...value,
+                        from: Number.parseInt(e.target.value.trim()),
+                      })
+                    }
+                  />
+                </Col>
 
-      {(["St. Louis", "Atlanta", "Mobile"] as CityName[]).map((cityName) => (
-        <MyChart
-          key={cityName}
-          cityName={cityName}
-          showApparentHighs={showApparentHighs}
-          showApparentLows={showApparentLows}
-          showActualHighs={showHighs}
-          showActualLows={showLows}
-          ranges={[red, green, blue]}
-        />
-      ))}
+                <Col>
+                  <input
+                    value={value.to}
+                    onChange={(e) =>
+                      setter({
+                        ...value,
+                        to: Number.parseInt(e.target.value.trim()),
+                      })
+                    }
+                  />
+                </Col>
+              </Row>
+            ))}
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="data">
+          <Accordion.Header>Data</Accordion.Header>
+
+          <Accordion.Body>
+            {(["St. Louis", "Atlanta", "Mobile"] as CityName[]).map(
+              (cityName) => (
+                <MyChart
+                  key={cityName}
+                  cityName={cityName}
+                  showApparentHighs={showApparentHighs}
+                  showApparentLows={showApparentLows}
+                  showActualHighs={showHighs}
+                  showActualLows={showLows}
+                  ranges={[red, green, blue]}
+                />
+              )
+            )}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </Container>
   );
 }
